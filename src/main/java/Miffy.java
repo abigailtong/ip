9a0783128b1fb9a664e65a026e167643029369bb
Abigail Tong <abigail@Abigails-MacBook-Air.local>
2026-01-20 18:47:24 +0800

    Rename Duke to Miffy

---
+++ src/main/java/Miffy.java
@@ -0,0 +1,13 @@
+public class Miffy {
+    public static void main(String[] args) {
+        String logo = " __  __ ___ _____ _____ __   __\n"
+                + "|  \\/  |_ _|  ___|  ___| \\ \\ / /\n"
+                + "| |\\/| || || |_  | |_   \\ \\ V / \n"
+                + "| |  | || ||  _| |  _|   | | |  \n"
+                + "|_|  |_|___|_|   |_|     |_| |_| \n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+}
