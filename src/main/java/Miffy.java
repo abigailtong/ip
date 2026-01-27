9c88819b2497ecdba8ee92a17fdd8f9901613da7
Abigail Tong <abigail@Abigails-MacBook-Air.local>
2026-01-27 18:22:48 +0800

    Add ability to Echo

--- src/main/java/Miffy.java
+++ src/main/java/Miffy.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Miffy {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         String logo = " __  __ ___ _____ _____ __   __\n"
                 + "|  \\/  |_ _|  ___|  ___| \\ \\ / /\n"
                 + "| |\\/| || || |_  | |_   \\ \\ V / \n"
@@ -8,6 +10,17 @@ public class Miffy {
         System.out.println("Hello from\n" + logo);
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        while(true) {
+            String input = in.nextLine();
+
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
